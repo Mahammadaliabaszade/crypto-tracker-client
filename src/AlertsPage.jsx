519a548036455c913acef59cc51679b6b1636339
Mahammadali Abaszada <Mahammadali.Abaszada@edu.rtu.lv>
2026-06-17 16:04:35 +0300

    Fix API URL

--- src/AlertsPage.jsx
+++ src/AlertsPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { getAlerts, addAlert, deleteAlert } from './api';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5259/api';
+const API_URL = 'https://cryptotracker-pg3h.onrender.com/api';
 const getToken = () => localStorage.getItem('token');
 const authHeader = () => ({ headers: { Authorization: `Bearer ${getToken()}` } });
 
